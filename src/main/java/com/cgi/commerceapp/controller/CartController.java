7443183c5a17b1ed84595546b3c838035e51b452
drake.menard <drake.menard@cgi.com>
2022-06-01 16:41:25 -0500

    edits

--- src/main/java/com/cgi/commerceapp/controller/CartController.java
+++ src/main/java/com/cgi/commerceapp/controller/CartController.java
@@ -7,9 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import com.cgi.commerceapp.exceptions.CartWithTheIDAlreadyExistsException;
 import com.cgi.commerceapp.exceptions.CartWithTheIDDoesntExistException;
--- src/main/java/com/cgi/commerceapp/exceptions/CartWithTheIDAlreadyExists.java
+++
@@ -1,5 +0,0 @@
-package com.cgi.commerceapp.exceptions;
-
-public class CartWithTheIDAlreadyExists extends Exception {
-
-}
